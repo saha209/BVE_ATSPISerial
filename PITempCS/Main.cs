5da8db4a990e5aac2297ca1f418c619610e6974e
saha209 <saha.209.kame@gmail.com>
2020-10-27 06:21:11 +0900

    初期案

---
+++ PITempCS/Main.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
+
+namespace PITempCS
+{
+
+    /// <summary>メインの機能をここに実装する。</summary>
+    public class Main
+    {
+        [DllImport("kernel32.dll", EntryPoint = "GetPrivateProfileStringW", CharSet = CharSet.Unicode, SetLastError = true)]
+        static extern uint GetPrivateProfileString(string lpAppName, string lpKeyName, string lpDefault, StringBuilder lpReturnedString, uint nSize, string lpFileName);
+
+        private static SerialPort myPort = null;
+        private static StringBuilder COMnumber;
+        private static float num;
+
+        static internal void Load()
+        {
+
+            openini();
+            openPort();
+
+            if (myPort != null)
+            {
+                try
+                {
+                    myPort.Write(new byte[] { 0 }, 0, 1);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "ATSPISerial", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        static internal void Dispose()
+        {
+            closePort();
+
+        }
+
+        static internal void GetVehicleSpec(Spec s)
+        {
+
+        }
+
+        static internal void Initialize(int s)
+        {
+
+        }
+        static unsafe internal void Elapse(State st, int* Pa, int* Sa)
+        {
+            num = State.V;
+            if (myPort == null)
+            {
+                return;
+            }
+            try
+            {
+                String data = num.ToString();
+                //! シリアルポートからテキストを送信する.
+                myPort.Write(data);
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ATSPISerial", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        static internal void SetPower(int p)
+        {
+
+        }
+
+        static internal void SetBrake(int b)
+        {
+
+        }
+
+        static internal void SetReverser(int r)
+        {
+
+        }
+        static internal void KeyDown(int k)
+        {
+
+        }
+
+        static internal void KeyUp(int k)
+        {
+
+        }
+
+        static internal void DoorOpen()
+        {
+
+        }
+        static internal void DoorClose()
+        {
+
+        }
+        static internal void HornBlow(int h)
+        {
+
+        }
+        static internal void SetSignal(int s)
+        {
+
+        }
+        static internal void SetBeaconData(Beacon b)
+        {
+
+        }
+
+        static internal void openini()
+        {
+            int capacitySize = 256;
+
+            COMnumber = new StringBuilder(capacitySize);
+            uint ret = GetPrivateProfileString("Data", "COM", "none", COMnumber, Convert.ToUInt32(COMnumber.Capacity), AppDomain.CurrentDomain.BaseDirectory + "ATSPISerial.ini");
+        }
+
+        static internal void openPort()
+        {
+            try
+            {
+                myPort = new SerialPort("COM" + COMnumber.ToString(), 19200, Parity.None, 8, StopBits.One);
+                myPort.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ATSPISerial", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                closePort();
+            }
+        }
+
+        static internal void closePort()
+        {
+            if (myPort != null)
+            {
+                try
+                {
+                    myPort.Close();
+                }
+                catch { }
+                try
+                {
+                    myPort.Dispose();
+                }
+                catch { }
+                myPort = null;
+            }
+        }
+    }
+}
